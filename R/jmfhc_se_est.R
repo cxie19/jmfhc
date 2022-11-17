3c452b39a3755015b5002d118b6396df64481976
cxie19 <90215497+cxie19@users.noreply.github.com>
2022-11-17 15:04:47 -0600

    Update jmfhc_se_est.R

--- R/jmfhc_se_est.R
+++ R/jmfhc_se_est.R
@@ -10,7 +10,7 @@
 #' @param beta_variable the variable name(s) defined as long-term covariate(s) in the model, which cannot be NULL.
 #' @param gamma_variable the variable name(s) defined as short-term covariate(s) in the model. By default gamma_variable = NULL.
 #' If there is no defined short-term covariate, the joint model becomes a proportional hazards cure submodel.
-#' @param fu_measure the variable name of repeatedly measured outcome (e.g., a biomarker).
+#' @param fu_measure the variable name(s) of fractional polynomials of repeatedly measured outcome (e.g., a biomarker).
 #' @param fu_time_variable the variable name of measurement times for the longitudinal measurements.
 #' @param max.int maximum number of iterations. The default is 200.
 #' @param no_cores the number of cores used during each point estimation for one resampled data set. The default is 5.
