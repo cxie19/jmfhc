047d2f9d83a783bcba35df9f64ddaa5a9006803c
cxie19 <90215497+cxie19@users.noreply.github.com>
2022-11-17 19:17:18 -0600

    Update est_surv_func.R

--- R/est_surv_func.R
+++ R/est_surv_func.R
@@ -3,6 +3,7 @@
 #' @param object an object of jmfhc_point_est.
 #' @param z_value value(s) of the long-term baseline covariate(s) in the order specified at the argument beta_variable of jmfhc_point_est() function.
 #' @param x_value value(s) of the short-term baseline covariate(s) in the order specified at the argument gamma_variable of jmfhc_point_est() function.
+#' If gamma is set as NULL in the jmfhc_point_est() function, then there is no need to put values here. The default is NULL.
 #' @param event_time the variable name of observed survival time in the data.
 #' @param n.random the number of randomly generated random effects based on the estimated covariance matrix for random effects. The default is 1000.
 #'
@@ -19,24 +20,30 @@
 #' survival_trt1 <- est_surv_func(object=result_coef,z_value=1,x_value=1,event_time="event.time")
 #' @import mvtnorm
 
-est_surv_func <- function(object,z_value,x_value,event_time,n.random=1000){
-
+est_surv_func <- function(object,z_value,x_value=NULL,event_time,n.random=1000){
+  
   set.seed(1)
-
+  
   coef <- object$coef
   Sigma <- object$re_cov
   length_re <- nrow(Sigma)
   betas <- coef[seq(1+length(z_value)+length_re)]
-  gammas <- coef[length(2+length(z_value)+length_re):length(2+length(z_value)+length_re+length(x_value))]
   dat_base <- object$dat_baseline
   F0t <- dat_base$base_cdf[order(dat_base[,event_time])]
-
   mvn <- mvrnorm(n.random, mu = rep(0,length_re), Sigma = Sigma)
-
-  est_survival <- sapply(seq(n.random),function(x)
-    exp(-rep(exp(betas%*%c(1,z_value,mvn[x,])),length(F0t))*(F0t^rep(exp(gammas%*%x_value),length(F0t)))))
+  
+  if (!is.null(x_value)){
+    gammas <- coef[(1+length(z_value)+length_re)+seq(x_value)]
+    est_survival <- sapply(seq(n.random),function(x)
+      exp(-rep(exp(betas%*%c(1,z_value,mvn[x,])),length(F0t))*(F0t^rep(exp(gammas%*%x_value),length(F0t)))))
+  }else{
+    est_survival <- sapply(seq(n.random),function(x)
+      exp(-rep(exp(betas%*%c(1,z_value,mvn[x,])),length(F0t))*F0t))
+  }
+  
   survival_mean <- apply(est_survival,1,mean)
-
+  
   return(data.frame(time=sort(dat_base[,event_time]),survival=survival_mean))
-
+  
 }
+
